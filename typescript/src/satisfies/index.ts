8aa3bf98334db6d6fc7717c67b4cf8a7ddf24ba3
Omar De Angelis <omar.hpvfilm@gmail.com>
2024-02-27 21:46:48 +0100

    satisfies

---
+++ typescript/src/satisfies/index.ts
@@ -0,0 +1,20 @@
+const user:Record<string, string> = {}
+// const user = {} satisfies Record<string, string>
+
+user.name = 'John'
+user.age = '30'
+
+// user.age = 30 // non auto complete
+
+// Example 2
+
+type Config = Record<string, string | number>
+
+// Use satisfies to check if the object satisfies the type & narrow down the type
+const config ={
+    expiresIn: 60,
+    secret: 'secret'
+} satisfies Config
+
+config.expiresIn
+
