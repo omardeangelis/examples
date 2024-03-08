38e21628d92097e4500b7106e8af1491b491547f
Omar De Angelis <omar.hpvfilm@gmail.com>
2024-03-08 14:38:00 +0100

    rename h1 in optimistic

--- next/optimistic-ui/start/app/page.tsx
+++ next/optimistic-ui/start/app/page.tsx
@@ -3,7 +3,7 @@ import { UserForm } from "./_components/RegisterForm";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center  p-24">
-      <h1 className="text-4xl font-bold">Form Management</h1>
+      <h1 className="text-4xl font-bold">Optimistic Update</h1>
       <div className="flex flex-col items-center justify-center mt-8 w-full">
         <UserForm />
       </div>
